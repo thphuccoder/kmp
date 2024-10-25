73a941f8ea54d0e882c0a639f13f971f5ad4150e
phuc <huyphuc0906@gmail.com>
2024-10-25 13:56:25 +0700

    Create project: Bottom Nav

---
+++ Bottom Nav/composeApp/src/iosMain/kotlin/tech/bumbii/kmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package tech.bumbii.kmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ Bottom Nav/shared/src/commonMain/kotlin/tech/bumbii/kmp/Platform.kt
@@ -0,0 +1,7 @@
+package tech.bumbii.kmp
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
