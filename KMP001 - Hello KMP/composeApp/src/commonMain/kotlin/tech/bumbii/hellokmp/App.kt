adc204d6b25cf0ce38e10b82250d128d8760187c
phuc <huyphuc0906@gmail.com>
2024-11-04 14:22:30 +0700

    Refactor

--- KMP001 - Hello KMP/composeApp/src/commonMain/kotlin/tech/bumbii/hellokmp/App.kt
+++ KMP001 - Hello KMP/composeApp/src/commonMain/kotlin/tech/bumbii/hellokmp/App.kt
@@ -1,12 +1,10 @@
 package tech.bumbii.hellokmp
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
